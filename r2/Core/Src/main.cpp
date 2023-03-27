9788d5a9894ce20b8d39402e425137a2bc48b3f7
Scott Sullivan <scott@sullysbrain.com>
2023-03-27 00:41:26 -0400

    Git commit from within STM32CubeIDE

--- r2/Core/Src/main.cpp
+++ r2/Core/Src/main.cpp
@@ -95,7 +95,7 @@ int main(void)
 	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 	  HAL_Delay(600);
 	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-	  HAL_Delay(10);
+	  HAL_Delay(25);
       /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
